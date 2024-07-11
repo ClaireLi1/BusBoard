88b7677cd42a1c192db6d594a10868cc69b88036
claireli <claire.li@softwire.com>
2024-07-11 17:46:16 +0100

    sort bus info when fetching bus info, instead of inside bus prediction.

--- bus_board_part2.rb
+++ bus_board_part2.rb
@@ -21,17 +21,18 @@ class BusStop
 
   def initialize(stop_id)
     @stop_id = stop_id
-    @buses_info = nil
+    @buses_info_sorted = nil
   end
 
   def fetch_buses_info
     web_address = "https://api.tfl.gov.uk/StopPoint/#{@stop_id}/Arrivals"
-    @buses_info = web_request(web_address)
+    buses_info = web_request(web_address)
+    @buses_info_sorted = buses_info.sort_by { |bus| bus["timeToStation"] }
   end
 
   def bus_prediction(number_of_buses)
-    sorted_buses = @buses_info.sort_by { |bus| bus["timeToStation"] }
-    next_buses = sorted_buses.first(number_of_buses)
+
+    next_buses = @buses_info_sorted.first(number_of_buses)
 
     next_buses.each do |bus|
       puts "StopID: #{@stop_id}, Route: #{bus['lineId']}, Destination: #{bus['destinationName']}, Arrives in: #{bus['timeToStation']/60} minutes"
